2c2ba706cc53d7bb7e6480bf1c5c2f66c1e12cc3
David Ebert <ebertek@gmail.com>
2018-05-24 12:09:23 +0200

    Change colors, add GDPR disclaimer

--- alairas.php
+++ alairas.php
@@ -39,16 +39,16 @@
 
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Gépbér</title>
+    <title>Gépbér – <?php echo "$name"; ?></title>
   </head>
 
-  <body style="font-size: 11px; font-family: Verdana, 'Bitstream Vera Sans', 'DejaVu Sans', Tahoma, Geneva, Arial, sans-serif; color: #636466; -webkit-font-smoothing: antialiased;">
+  <body style="font-size: 11px; font-family: Verdana, 'Bitstream Vera Sans', 'DejaVu Sans', Geneva, Tahoma, Arial, sans-serif; color: #515151; -webkit-font-smoothing: antialiased;">
 
-    <div style="font-size: 11px; font-family: Verdana, 'Bitstream Vera Sans', 'DejaVu Sans', Tahoma, Geneva, Arial, sans-serif; color: #636466; -webkit-font-smoothing: antialiased;">
-      <div style="color: #c41425; font-weight: 900;">
+    <div style="font-size: 11px; font-family: Verdana, 'Bitstream Vera Sans', 'DejaVu Sans', Geneva, Tahoma, Arial, sans-serif; color: #515151; -webkit-font-smoothing: antialiased;">
+      <div style="color: #be1128; font-weight: 900;">
         <p style="margin: 0px; text-transform: uppercase;"><?php echo "$name"; ?></p>
       </div>
-      <div style="color: #636466;">
+      <div style="color: #515151;">
         <p style="margin: 0px;"><span style="font-weight: 900;"><?php echo "$position"; if ($position_en != '') { echo (' / ' . $position_en); } ?></span></p>
         <p style="margin: 0px;">&nbsp;</p>
         <?php if ($t != '') { echo('<p style="margin: 0px;">T ' . $t . '</p>'); } ?>
@@ -57,10 +57,10 @@
         <p style="margin: 0px;"><a href="mailto:<?php echo "$e"; ?>"><?php echo "$e"; ?></a></p>
       </div>
       <a href="https://www.gepber.hu/"><img id="logo" style="width: 128px; margin: 6px 0px 6px 0px; padding: 2px 0px 2px 0px; border: 0px;" src="https://www.gepber.hu/img/gepber_128.png" alt="Gépbér logó" /></a>
-      <div style="color: #c41425; font-weight: 900;">
+      <div style="color: #be1128; font-weight: 900;">
         <p style="margin: 0px; text-transform: uppercase;">Gépbér Hungária Kft.</p>
       </div>
-      <div style="color: #636466;">
+      <div style="color: #515151;">
         <?php if ($location == 'D') { echo('<p style="margin: 0px;">4031 Debrecen • Richter Gedeon u. 0367/71 hrsz.</p>'); } ?>
         <?php if ($location == 'Gy') { echo('<p style="margin: 0px;">9028 Győr • Juharfa u. 1.</p>'); } ?>
         <?php if ($location != 'Sz') { echo('<p style="margin: 0px;">• Központ:</p>'); } ?>
@@ -69,16 +69,18 @@
         <p style="margin: 0px;"><a href="https://www.gepber.hu/">www.gepber.hu</a></p>
       </div>
       <div>
-        <a href="http://www.ipaf.org"><img id="ipaf" style="width: 48px; border: 0px; vertical-align: middle;" src="https://www.gepber.hu/img/ipaf.jpg" alt="IPAF Member" /></a>
+        <a href="https://www.ipaf.org"><img id="ipaf" style="width: 48px; border: 0px; vertical-align: middle;" src="https://www.gepber.hu/img/ipaf.jpg" alt="IPAF Member" /></a>
         <a href="https://www.facebook.com/gepberhungaria"><img id="fb" style="width: 24px; border: 0px; vertical-align: middle;" src="https://www.gepber.hu/img/fb.png" alt="Facebook" /></a>
       </div>
       <div style="color: #999999;">
         <hr style="border: 0px; color: #999999; background-color: #999999; height: 1px;" />
-        <p style="margin: 0px;"><i>A Gépbér Hungária Kft. Általános Szerződéses Feltételei felelősségkorlátozást tartalmaznak, továbbá alkalmazásra kell, hogy kerüljenek minden egyes, a Gépbér Hungária Kft.-vel kapcsolatos árajánlat, szerződés, valamint egyéb jogviszony esetén. A szóban forgó általános szerződéses feltételek hozzáférhetők, kinyomtathatók, valamint letölthetők a <a href="https://www.gepber.hu/">www.gepber.hu</a> címről.</i></p>
-        <p style="margin: 0px;"><i>A Gépbér Hungária Kft. bejegyzett székhelye Kecskeméten található, továbbá a cég a 03-09-108514-es cégjegyzékszámon szerepel a Bács-Kiskun Megyei Kereskedelmi és Iparkamara Cégnyilvántartásában.</i></p>
+        <p style="margin: 0px;"><i>A Gépbér Hungária Kft. Általános Szerződéses Feltételei felelősségkorlátozást tartalmaznak, továbbá alkalmazásra kell, hogy kerüljenek minden egyes, a Gépbér Hungária Kft.-vel kapcsolatos árajánlat, szerződés, valamint egyéb jogviszony esetén. A szóban forgó általános szerződéses feltételek hozzáférhetők, kinyomtathatók, valamint letölthetők a <a href="https://gepber.hu/hu/downloads">www.gepber.hu</a> címről.</i><br />
+        <i>A Gépbér Hungária Kft. bejegyzett székhelye Kecskeméten található, továbbá a cég a 03-09-108514-es cégjegyzékszámon szerepel a Bács-Kiskun Megyei Kereskedelmi és Iparkamara Cégnyilvántartásában.</i></p>
         <p style="margin: 0px;">&nbsp;</p>
-        <p style="margin: 0px;"><i>Gépbér Hungária Kft. general terms and condition contain a limitation of liability and are applicable to all quotations, contracts and other legal relationships with Gépbér Hungária Kft. These general terms and conditions can be accessed, printed and downloaded at <a href="https://www.gepber.hu/">www.gepber.hu</a>.</i></p>
-        <p style="margin: 0px;"><i>Gépbér Hungária Kft. has its registered office in Kecskemét and is listed in the Commercial Register of the Chamber of Commerce in Bács-Kiskun under file number 03-09-108514.</i></p>
+        <p style="margin: 0px;"><i>Gépbér Hungária Kft. general terms and condition contain a limitation of liability and are applicable to all quotations, contracts and other legal relationships with Gépbér Hungária Kft. These general terms and conditions can be accessed, printed and downloaded at <a href="https://gepber.hu/en/downloads">www.gepber.hu</a>.</i><br />
+        <i>Gépbér Hungária Kft. has its registered office in Kecskemét and is listed in the Commercial Register of the Chamber of Commerce in Bács-Kiskun under file number 03-09-108514.</i></p>
+        <p style="margin: 0px;">Disclaimer: <a href="https://www.tvh.com/glob/en/email-disclaimer">https://www.tvh.com/glob/en/email-disclaimer</a> <br />
+          This message is delivered to all addressees subject to the conditions set forth in the attached disclaimer, which is an integral part of this message.</p>
         <hr style="border: 0px; color: #999999; background-color: #999999; height: 1px;" />
       </div>
     </div>
