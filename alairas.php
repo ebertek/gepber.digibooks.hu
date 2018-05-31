0e55537ad0e15a9952586cb00ce138e570fca246
David Ebert <ebertek@gmail.com>
2018-05-31 13:49:01 +0200

    Lowercase id, fix position mismatch, fix indentation

--- alairas.php
+++ alairas.php
@@ -8,7 +8,7 @@
     $adatok[] = array_combine($header, $adat);
   }
 
-  $id            = $_GET['id'];
+  $id            = strtolower($_GET['id']);
   $key           = array_search($id, array_column($adatok, 'id'));
   if ($id != '' && $key !== FALSE) {
     $name        = $adatok[$key]['name'];
@@ -26,6 +26,10 @@
     $m           = $_GET['m'];
     $e           = $_GET['e'];
     $location    = $_GET['location'];
+  }
+  if ($position == '' && $position_en != '') {
+    $position = $position_en;
+    $position_en = '';
   }
 
   function telClean($phone) {
@@ -68,8 +72,8 @@
       <div style="color: #515151;">
         <p style="margin: 0px;"><span style="font-weight: 900;"><?php echo "$position"; if ($position_en != '') { echo (' / ' . $position_en); } ?></span></p>
         <p style="margin: 0px;">&nbsp;</p>
-        <?php if ($t != '') { echo('<p style="margin: 0px;">T ' . $t . '</p>' . PHP_EOL); }
-              if ($m != '') { echo('<p style="margin: 0px;">M ' . $m . '</p>' . PHP_EOL); } ?>
+        <?php if ($t != '') { echo('<p style="margin: 0px;">T ' . $t . '</p>' . PHP_EOL); } ?>
+        <?php if ($m != '') { echo('<p style="margin: 0px;">M ' . $m . '</p>' . PHP_EOL); } ?>
         <p style="margin: 0px;"><a href="mailto:<?php echo "$e"; ?>"><?php echo "$e"; ?></a></p>
       </div>
       <a href="https://www.gepber.hu/"><img id="logo" style="width: 128px; margin: 6px 0px 6px 0px; padding: 2px 0px 2px 0px; border: 0px;" src="https://www.gepber.hu/img/gepber_128.png" alt="Gépbér logó" /></a>
@@ -77,18 +81,18 @@
         <p style="margin: 0px; text-transform: uppercase;">Gépbér Hungária Kft.</p>
       </div>
       <div style="color: #515151;">
-        <?php if ($location == 'D') {
-                echo('<p style="margin: 0px;">4031 Debrecen • Richter Gedeon u. 0367/71 hrsz.</p>' . PHP_EOL);
-              } else
-              if ($location == 'Gy') {
-                echo('<p style="margin: 0px;">9028 Győr • Égerfa u. 8.</p>' . PHP_EOL);
-              } else
-              if ($location == 'Szombathely') {
-                echo('<p style="margin: 0px;">9700 Szombathely • Sági út 4.</p>' . PHP_EOL);
-              }
-              if ($location != 'Sz') {
-                echo('<p style="margin: 0px;">• Központ:</p>' . PHP_EOL);
-              } ?>
+<?php if ($location == 'D') {
+        echo('        <p style="margin: 0px;">4031 Debrecen • Richter Gedeon u. 0367/71 hrsz.</p>' . PHP_EOL);
+      } else
+      if ($location == 'Gy') {
+        echo('        <p style="margin: 0px;">9028 Győr • Égerfa u. 8.</p>' . PHP_EOL);
+      } else
+      if ($location == 'Szombathely') {
+        echo('        <p style="margin: 0px;">9700 Szombathely • Sági út 4.</p>' . PHP_EOL);
+      }
+      if ($location != 'Sz') {
+        echo('        <p style="margin: 0px;">• Központ:</p>' . PHP_EOL);
+      } ?>
         <p style="margin: 0px;"><?php if ($location != 'Sz') { echo('&nbsp;&nbsp;&nbsp;&nbsp;'); } ?>2310 Szigetszentmiklós • Leshegy út 9/d.</p>
         <p style="margin: 0px;"><?php if ($location != 'Sz') { echo('&nbsp;&nbsp;&nbsp;&nbsp;'); } ?>T +36 24 222 222 • F +36 24 222 221</p>
         <p style="margin: 0px;"><a href="https://www.gepber.hu/">www.gepber.hu</a></p>
